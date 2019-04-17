f1367fcd5a87f67aa1ea8695f76a5c9bbf1d5c57
carrieheyen@gmail.com <carrieheyen@gmail.com>
2019-04-17 18:22:54 -0500

    Add project files.

---
+++ StudioClass13_MVCBonjour/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StudioClass13_MVCBonjour.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
